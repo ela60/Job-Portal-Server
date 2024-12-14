da96074ebf50d2bae8a5a7e5a3c428c7a6846be2
Ambia-Ela <ambiaela7275@gmail.com>
2024-12-14 11:14:39 +0600

    get data

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -40,7 +40,15 @@ async function run() {
 
       app.get('/jobs', async (req, res) => {
           const cursor = jobsCollection.find();
-          const result=await cursor
+          const result = await cursor.toArray();
+          res.send(result);
+      });
+
+      app.get('/jobs/:id', async (req, res) => {
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) }
+          const result = await jobsCollection.findOne(query);
+          res.send(result);
       })
       
 
